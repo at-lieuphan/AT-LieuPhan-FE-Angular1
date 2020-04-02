282512208da324f6d769ded4f396b23e594ce8dc
at-lieuphan <lieu.phan@asiantech.vn>
2020-04-02 22:24:31 +0700

    change register form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NewsComponent } from './news/news.component';
+import { RegisterFormComponent } from './auth/components/register-form/register-form.component';
+
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'news', component: NewsComponent }
+  { path: 'news', component: NewsComponent },
+  { path: 'register', component: RegisterFormComponent}
 ];
 
 @NgModule({
