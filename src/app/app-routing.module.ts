6cb7d1dfcf007de702e7d65f406ffd373c9ce1a0
at-lieuphan <lieu.phan@asiantech.vn>
2020-04-03 21:46:25 +0700

    complete UI notFound page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NewsComponent } from './news/news.component';
 import { RegisterFormComponent } from './auth/components/register-form/register-form.component';
+import { NotFoundComponent } from './share/components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'news', component: NewsComponent },
-  { path: 'register', component: RegisterFormComponent}
+  { path: 'register', component: RegisterFormComponent},
+  { path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
